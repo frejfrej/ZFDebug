3fa3d922f0e867243be3816a1fc29be0386390df
Frej <github@oliana.net>
2014-12-28 23:28:20 +0100

    Locale plugin initial implementation

---
+++ library/ZFDebug/Controller/Plugin/Debug/Plugin/Locale.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * ZFDebug Zend Additions
+ *
+ * @category   ZFDebug
+ * @package    ZFDebug_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
+ * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
+ * @version    $Id$
+ */
+
+/**
+ * @category   ZFDebug
+ * @package    ZFDebug_Controller
+ * @subpackage Plugins
+ * @copyright  Copyright (c) 2008-2009 ZF Debug Bar Team (http://code.google.com/p/zfdebug)
+ * @license    http://code.google.com/p/zfdebug/wiki/License     New BSD License
+ */
+class ZFDebug_Controller_Plugin_Debug_Plugin_Locale extends ZFDebug_Controller_Plugin_Debug_Plugin implements ZFDebug_Controller_Plugin_Debug_Plugin_Interface
+{
+    /**
+     * Contains plugin identifier name
+     *
+     * @var string
+     */
+    protected $_identifier = 'locale';
+
+    /**
+     * @var Zend_Controller_Request_Abstract
+     */
+    protected $_request;
+
+    /**
+     * Create ZFDebug_Controller_Plugin_Debug_Plugin_Variables
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Gets identifier for this plugin
+     *
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return $this->_identifier;
+    }
+
+    /**
+     * Returns the base64 encoded icon
+     *
+     * @return string
+     **/
+    public function getIconData()
+    {
+        return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAFWSURBVBgZBcE/SFQBAAfg792dppJeEhjZn80MChpqdQ2iscmlscGi1nBPaGkviKKhONSpvSGHcCrBiDDjEhOC0I68sjvf+/V9RQCsLHRu7k0yvtN8MTMPICJieaLVS5IkafVeTkZEFLGy0JndO6vWNGVafPJVh2p8q/lqZl60DpIkaWcpa1nLYtpJkqR1EPVLz+pX4rj47FDbD2NKJ1U+6jTeTRdL/YuNrkLdhhuAZVP6ukqbh7V0TzmtadSEDZXKhhMG7ekZl24jGDLgtwEd6+jbdWAAEY0gKsPO+KPy01+jGgqlUjTK4ZroK/UVKoeOgJ5CpRyq5e2qjhF1laAS8c+Ymk1ZrVXXt2+9+fJBYUwDpZ4RR7Wtf9u9m2tF8Hwi9zJ3/tg5pW2FHVv7eZJHd75TBPD0QuYze7n4Zdv+ch7cfg8UAcDjq7mfwTycew1AEQAAAMB/0x+5JQ3zQMYAAAAASUVORK5CYII=';
+    }
+
+    /**
+     * Gets menu tab for the Debugbar
+     *
+     * @return string
+     */
+    public function getTab()
+    {
+        return ' Locale';
+    }
+
+    /**
+     * Gets content panel for the Debugbar
+     *
+     * @return string
+     */
+    public function getPanel()
+    {
+        $linebreak = $this->getLinebreak();
+        $locale = Zend_Registry::get('Zend_Locale');
+
+        $html = '<h4> View Locale info</h4>' . $linebreak;
+        $html .= 'Zend Locale is set to <strong>' . $locale . '</strong>' . $linebreak;
+
+        // adds form to clear the cache
+        $panel .= '<h4>Change Locale</h4>' . $linebreak . '<form method="post"><input name="debug_locale" type="text" placeholder="Locale..." class="input-small"></input></form>';
+
+        return $html;
+    }
+
+    private function changeLocale()
+    {
+        // Changing locale via debug box, must be done before bootstrap
+        if (APPLICATION_ENV !== 'production'){
+            if (isset($_POST['debug_locale']) && !empty($_POST['debug_locale'])){
+                $helper = new Yujia_Controller_Action_Helper_Locale();
+                $helper->locale($_POST['debug_locale']);
+            }
+
+            if (isset($_POST['clear_cache'])
+                    || ( isset($_GET['clear_cache']) && (APPLICATION_ENV !== 'production') )){
+                define('CLEAR_CACHE', true);
+            }
+        }
+    }
+
+}
